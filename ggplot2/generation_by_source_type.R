4bdd8c22764fa2bf3d5c99dbf0d18f1b19427bcf
laurencoppins <lmc7ud@virginia.edu>
2020-05-20 15:49:58 -0400

    fixed units

--- ggplot2/generation_by_source_type.R
+++ ggplot2/generation_by_source_type.R
@@ -59,7 +59,7 @@ series_list = data.frame(
 series_list$fuel<-as.character(series_list$fuel)
 
 
-VA_annual_energy_generation_mwh <- NULL
+VA_annual_energy_generation <- NULL
 
 for(row in 1:nrow(series_list)){
   table <- series_list[row,"series_id"]
@@ -68,41 +68,33 @@ for(row in 1:nrow(series_list)){
   dt <- get_EIA_series(eiaKey,table)
   setnames(dt,old="value",new=fuel)
   
-  if (is.null(VA_annual_energy_generation_mwh))
-  {VA_annual_energy_generation_mwh <- dt}
+  if (is.null(VA_annual_energy_generation))
+  {VA_annual_energy_generation <- dt}
   else
-  {VA_annual_energy_generation_mwh <-  merge(VA_annual_energy_generation_mwh, dt[], by ="year", all=TRUE)}
+  {VA_annual_energy_generation <-  merge(VA_annual_energy_generation, dt[], by ="year", all=TRUE)}
   
 }
 
-# Converting units to GWh
-VA_annual_energy_generation_gwh<-VA_annual_energy_generation_mwh[,`:=`(total_generation=total_generation/1000,
-                                                                       hydro_generation=hydro_generation/1000,
-                                                                       nuclear_generation=nuclear_generation/1000,
-                                                                       all_solar_generation=all_solar_generation/1000,
-                                                                       utility_solar_generation=utility_solar_generation/1000,
-                                                                       distributed_solar_generation=distributed_solar_generation/1000)]
-
-VA_annual_energy_generation_gwh[is.na(VA_annual_energy_generation_gwh)]<-0
+VA_annual_energy_generation[is.na(VA_annual_energy_generation)]<-0
 
 # Finding sum of total annual renewable generation
-VA_annual_energy_generation_gwh<- VA_annual_energy_generation_gwh%>%
+VA_annual_energy_generation<- VA_annual_energy_generation%>%
   mutate(renewable_generation=(hydro_generation+all_solar_generation))
 
 # Finding total annual renewable generation as a percent of total energy generation
-VA_annual_energy_generation_gwh<- VA_annual_energy_generation_gwh%>%
+VA_annual_energy_generation<- VA_annual_energy_generation%>%
   mutate(percent_renewable=((renewable_generation/total_generation)*100))
 
 # Finding sum of total annual carbon-free generation
-VA_annual_energy_generation_gwh<- VA_annual_energy_generation_gwh%>%
+VA_annual_energy_generation<- VA_annual_energy_generation%>%
   mutate(carbon_free_generation=hydro_generation+all_solar_generation+nuclear_generation)
 
 # Finding total annual carbon-free generation as a percent of total energy generation
-VA_annual_energy_generation_gwh<- VA_annual_energy_generation_gwh%>%
+VA_annual_energy_generation<- VA_annual_energy_generation%>%
   mutate(percent_carbon_free=((carbon_free_generation/total_generation)*100))
 
 # Graphing % of VA power generation (in GWh/yr) from renewables & carbon-free sources 
-percent_renewable_and_carbon_free<-VA_annual_energy_generation_gwh[,c(1,9,11)]
+percent_renewable_and_carbon_free<-VA_annual_energy_generation[,c(1,9,11)]
 melted_percent_renewable_and_carbon_free<-melt(percent_renewable_and_carbon_free,id="year")
 
 source(here::here("ggplot2","viz_functions.R"))
@@ -116,7 +108,7 @@ ggsave(path=path2graphics, filename="percent_renewable_and_carbon_free_line.png"
 
 
 # Solar, Hydro, and Nuclear Generation over Time
-carbon_free_generation_by_type<-VA_annual_energy_generation_gwh[,c(1,2,3,5,7)]
+carbon_free_generation_by_type<-VA_annual_energy_generation[,c(1,2,3,5,7)]
 melted_generation <- melt(carbon_free_generation_by_type,id="year")
 
 annual_carbon_free_generation_by_type_line<-line_figure(melted_generation,"Annual VA Energy Generation (GWh)","Annual Energy Generation in VA by Fuel Type",annual=TRUE,x_label="Year",subtitle_name=NULL,lower_limit=0)
@@ -127,7 +119,7 @@ ggsave(path=path2graphics, filename="annual_carbon_free_generation_by_type_line.
 
 
 # Solar (broken into distributed and utility), Hydro, and Nuclear Generation over Time
-carbon_free_generation_by_type2<-VA_annual_energy_generation_gwh[,c(1,2,3,4,6,7)]
+carbon_free_generation_by_type2<-VA_annual_energy_generation[,c(1,2,3,4,6,7)]
 melted_generation2 <- melt(carbon_free_generation_by_type2,id="year")
 
 annual_carbon_free_generation_by_type_line2<-line_figure(melted_generation2,"Annual VA Energy Generation (GWh)","Annual Energy Generation in VA by Type",annual=TRUE,x_label="Year",subtitle_name=NULL,lower_limit=0)
@@ -138,7 +130,7 @@ ggsave(path=path2graphics, filename="annual_carbon_free_generation_by_type_line2
 
 
 #Stacked Annual Carbon Free Generation Broken Out by Type
-carbon_free_generation_by_type3<-VA_annual_energy_generation_gwh[,c(1,3,5,7)]
+carbon_free_generation_by_type3<-VA_annual_energy_generation[,c(1,3,5,7)]
 melted_generation3 <- melt(carbon_free_generation_by_type3,id="year")
 
 carbon_free_generation_by_type_stacked<-stacked_area_figure(melted_generation3,"Annual VA Energy Generation (GWh)","Annual Energy Generation in VA by Type",annual=TRUE,x_label="Year",subtitle_name=NULL,lower_limit=0)
@@ -149,7 +141,7 @@ ggsave(path=path2graphics, filename="carbon_free_generation_by_type_stacked.png"
 
 
 # Stacked Annual Renewable Generation Broken Out by Type (hydro, utility solar, distributed solar)
-renewable_generation_by_type<-VA_annual_energy_generation_gwh[,c(1,3,4,6)]
+renewable_generation_by_type<-VA_annual_energy_generation[,c(1,3,4,6)]
 melted_generation4 <- melt(renewable_generation_by_type,id="year")
 
 renewable_generation_by_type_stacked<-stacked_area_figure(melted_generation4,"Annual VA Energy Generation (GWh)","Annual Renewable Generation in VA by Type",annual=TRUE,x_label="Year",subtitle_name=NULL,lower_limit=0)
@@ -160,9 +152,9 @@ ggsave(path=path2graphics, filename="renewable_generation_by_type_stacked.png")
 
 
 # Stacked Renewable versus Non-renewable Generation
-VA_annual_energy_generation_gwh<-VA_annual_energy_generation_gwh%>%
+VA_annual_energy_generation<-VA_annual_energy_generation%>%
   mutate(non_renewable_generation=(total_generation-renewable_generation))
-renewable_and_non_renewable<-VA_annual_energy_generation_gwh[,c(1,8,12)]
+renewable_and_non_renewable<-VA_annual_energy_generation[,c(1,8,12)]
 melted_renewable_and_non_renewable<-melt(renewable_and_non_renewable,id="year")
 
 renewable_versus_non_renewable_stacked<-stacked_area_figure(melted_renewable_and_non_renewable,"Annual VA Energy Generation (GWh)","Annual Renewable and Non-Renewable Generation in VA",annual=TRUE,x_label="Year",subtitle_name=NULL,lower_limit=0)
@@ -173,9 +165,9 @@ ggsave(path=path2graphics, filename="renewable_versus_non_renewable_stacked.png"
 
 
 # Stacked Carbon versus Carbon Free Generation
-VA_annual_energy_generation_gwh<-VA_annual_energy_generation_gwh%>%
+VA_annual_energy_generation<-VA_annual_energy_generation%>%
   mutate(carbon_generation=(total_generation-carbon_free_generation))
-carbon_and_carbon_free<-VA_annual_energy_generation_gwh[,c(1,10,13)]
+carbon_and_carbon_free<-VA_annual_energy_generation[,c(1,10,13)]
 melted_carbon_and_carbon_free<-melt(carbon_and_carbon_free,id="year")
 
 carbon_versus_carbon_free_stacked<-stacked_area_figure(melted_carbon_and_carbon_free,"Annual VA Energy Generation (GWh)","Annual Carbon and Carbon Free Generation in VA",annual=TRUE,x_label="Year",subtitle_name=NULL,lower_limit=0)
@@ -186,7 +178,7 @@ ggsave(path=path2graphics, filename="carbon_versus_carbon_free_stacked.png")
 
 
 # Total Renewable and Total Carbon Free Generation Over Time
-renewable_and_carbon_free<-VA_annual_energy_generation_gwh[,c(1,2,8,10)]
+renewable_and_carbon_free<-VA_annual_energy_generation[,c(1,2,8,10)]
 melted_renewable_and_carbon_free<-melt(renewable_and_carbon_free,id="year")
 
 renewable_and_carbon_free_line<-line_figure(melted_renewable_and_carbon_free,"Annual VA Energy Generation (GWh)","Annual Energy Generation in VA by Type",annual=TRUE,x_label="Year",subtitle_name=NULL,lower_limit=0)

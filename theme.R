af5f27640eb6737809e240454fd79919ff0581dd
laurencoppins <lmc7ud@virginia.edu>
2020-05-21 18:53:34 -0400

    turn theme and color into variables

--- theme.R
+++ theme.R
@@ -1,6 +1,7 @@
 #ggplot theme and scale code
 
-scale_color_brewer(palette="Set2")+ theme(plot.title = element_text(face = "bold", hjust=0.5)) + theme(
+library(RColorBrewer)
+theme1<- theme(plot.title = element_text(face = "bold", hjust=0.5)) + theme(
   panel.background = element_rect(fill = "white",
                                   colour = "white",
                                   size = 0.20, linetype = "solid"),
@@ -8,3 +9,7 @@ scale_color_brewer(palette="Set2")+ theme(plot.title = element_text(face = "bold
                                   colour = "gray"), 
   panel.grid.minor = element_line(size = 0.20, linetype = 'solid',
                                   colour = "gray"))
+
+color1<- scale_fill_brewer(palette="Set2")
+
+

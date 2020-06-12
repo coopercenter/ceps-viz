57e84752ec200bdf94bf0f71fe0783e1e36eba1d
Aishvarya Pathange <abp8hh@virginia.edu>
2020-06-12 10:17:00 -0400

    Updated visuals w 2019 data

--- ggplot2/capacity_factors.R
+++ ggplot2/capacity_factors.R
@@ -16,10 +16,18 @@ db_driver = dbDriver("PostgreSQL")
 source(here::here("my_postgres_credentials.R"))
 db <- dbConnect(db_driver,user=db_user, password=ra_pwd,dbname="postgres", host=db_host)
 
+#load in metadata
+metadata <- data.table(dbGetQuery(db,"select * from metadata ;"))
+
 #pulling 2018 generator data from database
-table="eia860_generator_y2018"
-script  <- paste0("select * from ",table," where \"State\"='VA';")
-generator_2018 <- data.table(dbGetQuery(db,script))
+generator_2018 <- data.table(dbGetQuery(db,"select \"Utility_ID\", \"Utility_Name\", \"Plant_Code\", \"Plant_Name\", \"Technology\", \"Nameplate_Capacity_MW\", \"Operating_Month\", \"Operating_Year\" from eia860_generator_y2018 where \"State\"='VA';"))
+
+#pulling in 2014-2017 data & 2019 early release data:
+generator_2014 <- data.table(dbGetQuery(db,"select \"Utility ID\", \"Utility Name\", \"Plant Code\", \"Plant Name\", \"Technology\", \"Nameplate Capacity (MW)\", \"Operating Month\", \"Operating Year\" from generator_2014 where \"State\"='VA';"))
+generator_2015 <- data.table(dbGetQuery(db,"select \"Utility ID\", \"Utility Name\", \"Plant Code\", \"Plant Name\", \"Technology\", \"Nameplate Capacity (MW)\", \"Operating Month\", \"Operating Year\" from generator_2015 where \"State\"='VA';"))
+generator_2016 <- data.table(dbGetQuery(db,"select \"Utility ID\", \"Utility Name\", \"Plant Code\", \"Plant Name\", \"Technology\", \"Nameplate Capacity (MW)\", \"Operating Month\", \"Operating Year\" from generator_2016 where \"State\"='VA';"))
+generator_2017 <- data.table(dbGetQuery(db,"select \"Utility ID\", \"Utility Name\", \"Plant Code\", \"Plant Name\", \"Technology\", \"Nameplate Capacity (MW)\", \"Operating Month\", \"Operating Year\" from generator_2017 where \"State\"='VA';"))
+generator_2019 <- data.table(dbGetQuery(db,"select \"Utility ID\", \"Utility Name\", \"Plant Code\", \"Plant Name\", \"Technology\", \"Nameplate Capacity (MW)\", \"Operating Month\", \"Operating Year\" from generator_2019_early_release where \"State\"='VA';"))
 
 fetch_time_series_from_db <- function(db_table_name, fuel_code, con){
   # Fetch generation data from a specified db table; return as a data table
@@ -67,85 +75,34 @@ for(row in 1:nrow(series_list)){
 
 dbDisconnect(db)
 
-#making column names lowercase
-names(generator_2018) <- tolower(names(generator_2018))
-
-#selecting relevant variables
-generator_2018 <- generator_2018[,.(utility_id,utility_name,plant_code,plant_name,technology,nameplate_capacity_mw,operating_month,operating_year)]
-
-#creating date-online variable for each generator
-generator_2018[,date_online:=paste0(operating_year,"-",operating_month,"-01")]
-generator_2018[,date_online:=as.Date(date_online)]
-
-#pulling in 2014-2017 data:
-readsheet <- function(file_path,range_dimensions){
-  library(readxl)
-  library(data.table)
-  #Data is from "https://www.eia.gov/electricity/data/eia860/xls/"
-  # Read just the column names from the spreadsheet
-  nms <- names(read_excel(file_path, n_max = 1, skip=1))
-  # Refashion column names into good variable names
+#reformatting column names in generator data & creating date_online variable for each generator with generator_clean function
+generator_clean <- function(generator_data){
+  
+  #refashion column names into good variable names
+  nms <- names(generator_data)
   good_names = gsub("[?())]","",nms)
   good_names = gsub("[[:punct:][:blank:]/]","_",good_names)
   good_names <- tolower(good_names)
-  # If you don't specify column types, read_excel will guess, often that's OK
-  # In this case, it doesn't work so well, so I specify the types
-  if(length(nms)==73){
-    column_types = c("numeric","text","numeric","text","text",
-                     "text","text","text","text","text",
-                     "text","text","text","text","text",
-                     "numeric","numeric", "numeric", "numeric", "numeric",
-                     "text", "numeric", "numeric", "text", "text",
-                     "numeric", "numeric","numeric", "numeric", "text",
-                     "text", "numeric", "text", "text", "text",
-                     "text", "text", "text", "text", "text",
-                     "text", "text", "text", "text", "text",
-                     "numeric", "text", "text", "text","text",
-                     "text", "text", "text", "text","numeric",
-                     "numeric", "numeric", "numeric", "numeric", "numeric",
-                     "numeric", "numeric", "text", "text", "numeric",
-                     "numeric", "numeric","text","numeric", "numeric",
-                     "text", "text", "text")
-  }
-  else{
-    column_types = c("numeric","text","numeric","text","text",
-                     "text","text","text","text","text",
-                     "text","text","text","text","text",
-                     "numeric","numeric", "numeric", "numeric", "numeric",
-                     "text", "numeric", "numeric", "text", "text",
-                     "numeric", "numeric","numeric", "numeric", "text",
-                     "text", "numeric", "text", "text", "text",
-                     "text", "text", "text", "text", "text",
-                     "text", "text", "text", "text", "text",
-                     "numeric", "text", "text", "text","text",
-                     "text", "text", "text", "text","numeric",
-                     "numeric", "numeric", "numeric", "numeric", "numeric",
-                     "numeric", "numeric", "text", "text", "numeric",
-                     "numeric", "numeric","text","numeric", "numeric",
-                     "text", "text")
-  }
-  # Read the data into a data.table
-  thisData = data.table(read_excel(file_path, col_types=column_types, range=range_dimensions))
-  #sheet=1,cell_cols(1:11)))
-  # Use the good variable names
-  setnames(thisData,good_names)
-  #only selecting VA data
-  thisData <- thisData[state=="VA"]
-  #only selecting relevant variables
-  thisData <- thisData[,.(utility_id,utility_name,plant_code,plant_name,technology,nameplate_capacity_mw,operating_month,operating_year)]
-  #creating date variable
-  thisData[,date_online:=paste0(operating_year,"-",operating_month,"-01")]
-  thisData[,date_online:=as.Date(date_online)]
-  return(thisData)
+  
+  #replace column names with better names
+  names(generator_data) <- good_names
+  
+  #creating date-online variable for each generator
+  generator_data[,date_online:=paste0(operating_year,"-",operating_month,"-01")]
+  generator_data[,date_online:=as.Date(date_online)]
+  
+  #casting nameplate variable as numeric
+  generator_data[,nameplate_capacity_mw:=as.numeric(nameplate_capacity_mw)]
+  
+  return(generator_data)
 }
 
-generator_2017 = readsheet(here::here("ggplot2","3_1_Generator_Y2017.xlsx"),"A2:BU21439")
-
-generator_2016 = readsheet(here::here("ggplot2","3_1_Generator_Y2016.xlsx"), "A2:BU20726")
-
-generator_2015 = readsheet(here::here("ggplot2","3_1_Generator_Y2015.xlsx"), "A2:BT20070")
-
-generator_2014 = readsheet(here::here("ggplot2","3_1_Generator_Y2014.xlsx"), "A2:BT19747")
+generator_2014 <- generator_clean(generator_2014)
+generator_2015 <- generator_clean(generator_2015)
+generator_2016 <- generator_clean(generator_2016)
+generator_2017 <- generator_clean(generator_2017)
+generator_2018 <- generator_clean(generator_2018)
+generator_2019 <- generator_clean(generator_2019)
 
 #summing nameplate (mw) additions by fuel type then using these additions to get nameplate over time
 nameplate_sums <- function(generator_data,data_year){
@@ -197,16 +154,18 @@ capacity_by_fuel_type_2015 <- nameplate_sums(generator_2015,2015)
 capacity_by_fuel_type_2016 <- nameplate_sums(generator_2016,2016)
 capacity_by_fuel_type_2017 <- nameplate_sums(generator_2017,2017)
 capacity_by_fuel_type_2018 <- nameplate_sums(generator_2018,2018)
+capacity_by_fuel_type_2019 <- nameplate_sums(generator_2019,2019)
 
 #adding zero-value solar_photovoltaic column to capacity by fuel type 2014 & 2015 tables so they can be merged
 capacity_by_fuel_type_2014[,solar_photovoltaic:=0]
 capacity_by_fuel_type_2015[,solar_photovoltaic:=0]
 
 #merging the capacity by fuel type tables for each year to get capacity by fuel type over time
-capacity_by_fuel_type <- merge(capacity_by_fuel_type_2017,capacity_by_fuel_type_2018,all=TRUE)
+capacity_by_fuel_type <- merge(capacity_by_fuel_type_2014,capacity_by_fuel_type_2015,all=TRUE)
 capacity_by_fuel_type <- merge(capacity_by_fuel_type,capacity_by_fuel_type_2016,all=TRUE)
-capacity_by_fuel_type <- merge(capacity_by_fuel_type,capacity_by_fuel_type_2015,all=TRUE)
-capacity_by_fuel_type <- merge(capacity_by_fuel_type,capacity_by_fuel_type_2014,all=TRUE)
+capacity_by_fuel_type <- merge(capacity_by_fuel_type,capacity_by_fuel_type_2017,all=TRUE)
+capacity_by_fuel_type <- merge(capacity_by_fuel_type,capacity_by_fuel_type_2018,all=TRUE)
+capacity_by_fuel_type <- merge(capacity_by_fuel_type,capacity_by_fuel_type_2019,all=TRUE)
 
 #merging data on capacity with data on generation to calculate capacity factors
 #       * only merging data beginning in 2013
@@ -224,8 +183,8 @@ capacity_factors[date>="2013-09-01",`:=`(other_waste_biomass=na.locf(other_waste
                                          wood_wood_waste_biomass=na.locf(wood_wood_waste_biomass))]
 
 #making capacity factor information begin in 2014 as this is first full year of data
-#making capacity factor information end in 2018 as that is last year of generator data
-capacity_factors <- capacity_factors[year(date)>=2014&year(date)<=2018]
+#making capacity factor information end in 2019 as that is last year of generator data
+capacity_factors <- capacity_factors[year(date)>=2014&year(date)<=2019]
 
 #adding days in month variable for capacity factor calculations purposes
 capacity_factors[,days_in_month:=days_in_month(date)]
@@ -239,25 +198,26 @@ capacity_factors[,`:=`(biomass_cf=other_biomass_gen_gwh/(other_waste_biomass/100
                        hydroelctric_cf=conventional_hydroelectric_gen_gwh/(conventional_hydroelectric/1000*24*days_in_month)*100,
                        wood_cf=wood_gen_gwh/(wood_wood_waste_biomass/1000*24*days_in_month)*100)]
 
+
 #-------------------------------PLOTTING CAPACITY FACTORS VISUALLY-----------------------------------------------
 
 source(here::here("ggplot2","viz_functions.R"))
 
 ####mean capacity factor of coal
-mean_coal_cf_2018<- capacity_factors[year(date)==2018,mean(coal_cf)]
+mean_coal_cf_2019<- capacity_factors[year(date)==2019,mean(coal_cf)]
 mean_coal_cf_2014<- capacity_factors[year(date)==2014,mean(coal_cf)]
 
 ####data tables for coal
-mean_coal_cf_2018_data_table<-data.table(year=2018,variable=c("coal", " "),value=c(mean_coal_cf_2018, 100-mean_coal_cf_2018))
+mean_coal_cf_2019_data_table<-data.table(year=2019,variable=c("coal", " "),value=c(mean_coal_cf_2019, 100-mean_coal_cf_2019))
 mean_coal_cf_2014_data_table<-data.table(year=2014,variable=c("coal", " "),value=c(mean_coal_cf_2014, 100-mean_coal_cf_2014))
 
 
-####2018 Pie chart
-title_name<- "Coal Average Capacity Factor 2018"
+####2019 Pie chart
+title_name<- "Coal Average Capacity Factor 2019"
 theme_colors <- c("orange","white")
-coal_cf_2018_piechart <- plot_ly(mean_coal_cf_2018_data_table,labels=~variable,values=~value,type='pie',textinfo="percent",hoverinfo="none",insidetextfont = list(color = 'white'),marker=list(colors=theme_colors),sort=F) %>%
+coal_cf_2019_piechart <- plot_ly(mean_coal_cf_2019_data_table,labels=~variable,values=~value,type='pie',textinfo="percent",hoverinfo="none",insidetextfont = list(color = 'white'),marker=list(colors=theme_colors),sort=F) %>%
   layout(title=list(text=title_name,x=0.55),showlegend=F,annotations=list(x=0.5,y=-0.1,text=paste0("<i>","U.S. Energy Information Administration","</i>"),showarrow=F,xref='paper',yref='paper',font=list(size=10))) 
-coal_cf_2018_piechart
+coal_cf_2019_piechart
 
 
 ######2014 Pie chart
@@ -270,26 +230,29 @@ coal_cf_2014_piechart
 
 #######Coal and Natural Gas Capacity Factors Over Time
 lf_coal_cf_and_gas_cf<- melt(capacity_factors[,.(date,coal_cf,natural_gas_cf)],id="date")
-coal_cf_and_gas_cf_over_time<-line_figure(lf_coal_cf_and_gas_cf, "Capacity Factor","Coal and Natural Gas Capacity Factors Over Time", annual=FALSE)
+coal_cf_and_gas_cf_over_time<-line_figure(lf_coal_cf_and_gas_cf, "Capacity Factor","Coal and Natural Gas Capacity Factors Over Time",annual=FALSE)
 coal_cf_and_gas_cf_over_time
-##need to add annotation
+coal_cf_and_gas_cf_over_time_p<- ggplotly_wrapper(coal_cf_and_gas_cf_over_time)
+
+####Coal and Natural Gas Capacity Factors Over Time Faceted
+coal_cf_and_gas_cf_over_time+facet_grid(rows=vars(variable))
 
 
 lf_all_capacity_factors<-melt(capacity_factors[,.(date,biomass_cf,coal_cf,natural_gas_cf,nuclear_cf,petroleum_liquids_cf,hydroelctric_cf, wood_cf)],id="date")
-mean_coal_cf_2018<- capacity_factors[year(date)==2018,mean(coal_cf)]
-mean_biomass_cf_2018<- capacity_factors[year(date)==2018,mean(biomass_cf)]
-mean_natural_gas_cf_2018<- capacity_factors[year(date)==2018,mean(natural_gas_cf)]
-mean_nuclear_cf_2018<- capacity_factors[year(date)==2018,mean(nuclear_cf)]
-mean_petroleum_liquids_cf_2018<- capacity_factors[year(date)==2018,mean(petroleum_liquids_cf)]
-mean_hydroelctric_cf_2018<- capacity_factors[year(date)==2018,mean(hydroelctric_cf)]
-mean_wood_cf_2018<- capacity_factors[year(date)==2018,mean(wood_cf)]
+mean_coal_cf_2019<- capacity_factors[year(date)==2019,mean(coal_cf)]
+mean_biomass_cf_2019<- capacity_factors[year(date)==2019,mean(biomass_cf)]
+mean_natural_gas_cf_2019<- capacity_factors[year(date)==2019,mean(natural_gas_cf)]
+mean_nuclear_cf_2019<- capacity_factors[year(date)==2019,mean(nuclear_cf)]
+mean_petroleum_liquids_cf_2019<- capacity_factors[year(date)==2019,mean(petroleum_liquids_cf)]
+mean_hydroelctric_cf_2019<- capacity_factors[year(date)==2019,mean(hydroelctric_cf)]
+mean_wood_cf_2019<- capacity_factors[year(date)==2019,mean(wood_cf)]
 ###data table excluding biomass bc value doesn't make sense
-mean_all_cf_2018_data_table<-data.table(year=2018,variable=c("coal", "natural gas","nuclear", "petroleum liquids", "hydroelectric","wood"),
-                                        value=c(mean_coal_cf_2018,mean_natural_gas_cf_2018,mean_nuclear_cf_2018,mean_petroleum_liquids_cf_2018,mean_hydroelctric_cf_2018,mean_wood_cf_2018))
+mean_all_cf_2019_data_table<-data.table(year=2019,variable=c("coal", "natural gas","nuclear", "petroleum liquids", "hydroelectric","wood"),
+                                        value=c(mean_coal_cf_2019,mean_natural_gas_cf_2019,mean_nuclear_cf_2019,mean_petroleum_liquids_cf_2019,mean_hydroelctric_cf_2019,mean_wood_cf_2019))
 
-###bar graph of average capacity factor of all fuel types in 2018
-capicity_factors_2018_bar<-ggplot(data=mean_all_cf_2018_data_table, aes(x=variable, y=value)) +
+###bar graph of average capacity factor of all fuel types in 2019
+capicity_factors_2019_bar<-ggplot(data=mean_all_cf_2019_data_table, aes(x=variable, y=value)) +
   geom_bar(stat="identity")+
-  xlab("Fuel Type")+ylab("Capacity Factor")+labs(title="Average Capacity Factor in 2018")
-capicity_factors_2018_bar
+  xlab("Fuel Type")+ylab("Capacity Factor")+labs(title="Average Capacity Factor in 2019")
+capicity_factors_2019_bar
 
